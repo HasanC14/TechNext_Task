42e0ca52f7f013b7a9c633d549a7fa01fc6e9a3c
HasanC14 <chowdhuryh999@gmail.com>
2024-02-10 02:13:19 +0600

    user fetched from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/users`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Fetch Failed");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data.users);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-semibold mb-4">User List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {users.map((user) => (
+          <div
+            key={user?.id}
+            className=" rounded-lg shadow-md p-6 bg_glass custom-border"
+          >
+            {/* Image */}
+            <img
+              src={user?.image}
+              alt={`${user?.firstName} ${user?.lastName}`}
+              className="w-16 h-16 rounded-full mx-auto mb-4"
+            />
+            {/* Name */}
+            <div className="text-lg font-semibold text-center mb-2">
+              {user?.firstName} {user?.lastName}
+            </div>
+            {/* Email */}
+            <div className="text-sm text-gray-600 text-center mb-2">
+              {user?.email}
+            </div>
+            {/* Address */}
+            <div className="text-sm text-gray-600 mb-2">
+              Address: {user?.address.address}, {user?.address.city}
+            </div>
+            {/* Company */}
+            <div className="text-sm text-gray-600">
+              Company: {user?.company.name}
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
